636f882593c4e11fecac34dc52707f3f267e6b73
Arjun Yadav <arjun.prakash.yadav@gmail.com>
2019-04-15 13:35:48 +0100

    commit 15th april morning

--- searchskillapp/src/App.js
+++ searchskillapp/src/App.js
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './css/pure-min.css';
 
 class App extends Component {
+  state = {
+    isLoading: true,
+    groups: {}
+  };
+
+  async componentDidMount() {
+    const response = await fetch('https://randomuser.me/api/?results=50');
+    const body = await response.json();
+    this.setState({ groups: body, isLoading: false });
+    console.log(body);
+  }
+
   render() {
+    const {groups, isLoading} = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="App-intro"> 
+                  <table class="pure-table">
+                   <thead>
+                    <tr>
+                      <th>First Name</th>
+                      <th>Last Name</th>
+                      <th>City</th>
+                    </tr>
+                   </thead>
+                   <tbody>
+                   {groups.results.map(group => {
+                     return (
+                     <tr key={group}>
+                      <td>{group.name.first}</td>
+                      <td>{group.name.last}</td>
+                      <td>{group.location.city}</td>
+                     </tr>
+                     )
+                     })
+                    }
+                   </tbody>
+                  </table>
+          </div>
         </header>
       </div>
     );
